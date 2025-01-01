bee2121322900b13931249f87ed9b4933853c1c3
bertoncellos <136335408+bertoncellos@users.noreply.github.com>
2025-01-01 19:57:23 +0000

    Cadastro das cartas - 1.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,21 +2,42 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+int main() 
+    {
+    // Declaração das Variáveis
+
+    int codigoCidade;
+    char nomeCidade[50];
+    int populacaoCidade;
+    float areaCidade;
+    float pibCidade;
+    int pontosTuristicosCidade;
+
+    // Leitura dos dados das cartas e gravação nas variáveis
+
+    printf("Insira o Código da Cidade: \n");
+    scanf("%d", &codigoCidade);
+
+    printf("Insira o Nome da Cidade: \n");
+    scanf("%s", &nomeCidade);
+
+    printf("Insira o Número de Habitantes da Cidade: \n");
+    scanf("%d", &populacaoCidade);
+
+    printf("Insira a Área da Cidade em km²: \n");
+    scanf("%f", &areaCidade);
+
+    printf("Insira o PIB da Cidade: \n");
+    scanf("%f", &pibCidade);
+
+    printf("Insira a quantidade de Pontos Turísticos da Cidade: \n");
+    scanf("%d", &pontosTuristicosCidade);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Impressão das informações das cartas
+
+    printf("O Código da Cidade é: %d \n O Nome da Cidade é: %s\n A Cidade %s tem: %d habitantes\n A Área da Cidade %s é de: %.2f km²\n O PIB da Cidade %s é de: R$%.2f\n A Cidade %s tem: %d Pontos Turísticos\n", codigoCidade, nomeCidade, nomeCidade, populacaoCidade, nomeCidade, areaCidade, nomeCidade, pibCidade, nomeCidade, pontosTuristicosCidade);
 
     return 0;
-}
+    }
